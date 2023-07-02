55d14dd6e4d1d18370538dbda986b19f7865746a
Ophélien DUPARC <ophelien.duparc@projetretro.io>
2023-07-02 16:25:48 +0200

    Refactor

--- lib/src/dao/provider_data_access_object.dart
+++ lib/src/dao/provider_data_access_object.dart
@@ -17,30 +17,4 @@ class ProviderDataAccessObject extends DataAccessObject<Provider> {
   factory ProviderDataAccessObject() {
     return _providerDataAccessObject ??= ProviderDataAccessObject._();
   }
-
-  /// Will return all [Provider] linked to the [Topic]'s [id]
-  Future<List<Provider>> getByTopicId(int id) async {
-    String uri = '$resourceUrl/$id/providers';
-    Uri url = Uri.parse(uri);
-
-    List<Provider> providers = <Provider>[];
-
-    var response = await client.get(url, headers: headers);
-
-    if (response.statusCode == 200) {
-      Map<dynamic, dynamic> decodedResponse = json.decode(response.body);
-
-      if (decodedResponse['data'].isEmpty) {
-        return providers;
-      }
-
-      for (var element in decodedResponse['data']) {
-        providers.add(Provider.fromJson(element));
-      }
-    } else {
-      throw Exception(response);
-    }
-
-    return providers;
-  }
 }
