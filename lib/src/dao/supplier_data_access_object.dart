7c8f4a79d7fc6f9b252cd6e4c7603a8a3a9f3025
Ophélien DUPARC <ophelien.duparc@projetretro.io>
2023-11-26 21:40:28 +0100

    Missing providerType in supplier

--- lib/src/dao/supplier_data_access_object.dart
+++ lib/src/dao/supplier_data_access_object.dart
@@ -8,4 +8,31 @@ final class SupplierDataAccessObject extends DataAccessObject<Supplier> {
   Supplier fromJson(Map<String, dynamic> json) {
     return Supplier.fromJson(json);
   }
+
+  Future<ProviderType> providerType(Supplier supplier) async {
+    final Uri uri = Uri.https(
+        MagnifiqueCoupleClient.host, '/api/suppliers/${supplier.id}/provider_type');
+
+    Response response;
+
+    try {
+      response = await _client.get(uri);
+    } on ClientException {
+      rethrow;
+    } on Exception {
+      rethrow;
+    }
+
+    ProviderType model;
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> json = jsonDecode(response.body)['data'];
+      model = ProviderType.fromJson(json);
+    } else {
+      throw MagnifiqueException(response);
+    }
+
+    return model;
+  }
+
 }
